55ba865965be438959a949a624eee62fae3dd0a4
Marcelo Alves <alvesma@gmail.com>
2022-02-26 12:00:40 -0300

    Sistema para gerar manifestos K8s

--- index.js
+++ index.js
@@ -52,6 +52,43 @@ app.post("/gerapod",(req,res) => {
     })
 
 })
+app.get("/geraryaml_service",(req,res) => {
+
+    res.render("geraryaml_service")
+
+})
+
+app.post("/geraservice",(req,res)=>{
+
+    var sapiversion = req.body.sapiversion
+    var skind = req.body.skind
+    var sapp = req.body.sapp
+    var sname = req.body.sname
+    var snamespace = req.body.snamespace
+    var stype = req.body.stype
+    var nport = req.body.nport
+    var port = req.body.port
+    var tport = req.body.tport
+    var ssapp = req.body.ssapp
+
+    res.render("geraservice",{
+
+        sapiversion: sapiversion,
+        skind: skind,
+        sapp: req.body.sapp,
+        sname: req.body.sname,
+        snamespace: snamespace,
+        stype: stype,
+        nport: nport,
+        port: port,
+        tport: tport,
+        ssapp: ssapp
+
+
+
+    })
+
+})
 
 app.listen(3000,()=>{console.log("servidor ok")
 })
